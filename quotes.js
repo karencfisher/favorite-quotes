2b89a3bafdae60c3f4ce84dd1969d52eb7c67867
karencfisher <karencfisher@hotmail.com>
2024-03-31 20:53:33 -0600

    refactor and add filtering out duplicates

--- quotes.js
+++ quotes.js
@@ -1,6 +1,9 @@
+/************************************ 
+*   query functionality 
+*/
 async function getQuotes(query) {
-    let url = baseURL;
-    
+    // fill out the URL
+    let url = baseURL;  
     if (query === "") {
         url += "random";
     }
@@ -8,6 +11,7 @@ async function getQuotes(query) {
         url += `search?query=${query}`;
     }
 
+    // make request
     let result = null;
     let contents = null;
     try {
@@ -19,38 +23,53 @@ async function getQuotes(query) {
     }
 
     if (initialState) {
-        // random quote
-        addQuotation(contents.content, contents.author)
+        // random quote on app opening
+        addQuotation(contents._id, contents.content, contents.author)
     }
     else {
+        // processing quotes from query
         const quotations = document.getElementById("quotations");
         quotations.innerHTML = "";
 
+        // filter out pinned duplicates
+        contents.results = contents.results.filter(isNotDuplicate);
+
         if (searchLevel < 4) {
-            // filter results to full or partial matches
+            // filter results for exact or partial matches
             contents.results = contents.results.filter((quote) => filterNames(quote, query, searchLevel));
         }
 
         // Add quotations to page
         contents.results.forEach((item) => {
-            addQuotation(item.content, item.author)
+            addQuotation(item._id, item.content, item.author)
         });
-        displayMessage(`${contents.results.length} quotations found for author "${query}"`, "message");
+        displayMessage(`${contents.results.length} new quotations found for author "${query}"`, "message");
     }
 }
 
+function isNotDuplicate(quote) {
+    // check for duplicate in pinned by id
+    const pinned = document.getElementById("pinned-quotations");
+    const quotes = pinned.querySelectorAll("blockquote");
+    for (let i = 0; i < quotes.length; i++) {
+        if (quotes[i].id === quote._id) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function filterNames(quote, query, level) {
-    // Complete match
+    const nameParts = quote.author.toLowerCase().split(" ");
+    const queryParts = query.toLowerCase().split(" ");
+
     if (level === 1) {
+        // Exact match
         if (quote.author.toLowerCase() === query.toLowerCase()) {
             return true;
         }
     }
-
-    const nameParts = quote.author.toLowerCase().split(" ");
-    const queryParts = query.toLowerCase().split(" ");
-
-    if (level === 2) {
+    else if (level === 2) {
         // Match last name
         const lastName = nameParts.slice(-1);
         const lastQueryName = queryParts.slice(-1);
@@ -58,8 +77,7 @@ function filterNames(quote, query, level) {
             return true;
         }
     }
-
-    if (level === 3) {
+    else if (level === 3) {
         // Match any name
         const namePartsSet = new Set(nameParts);
         const queryPartsSet = new Set(queryParts);
@@ -72,16 +90,22 @@ function filterNames(quote, query, level) {
     return false;
 }
 
-function addQuotation(quote, citation) {
-    const quotations = document.getElementById("quotations");
+function addQuotation(id, quote, citation) {
+    // Create blockquote element
     const blockquote = document.createElement("blockquote");                
     blockquote.classList.add("quotation");
+    blockquote.id = id;
     blockquote.tabIndex = "0";
 
+    // add icons to the quote
     const icons = document.createElement("div");
     icons.classList.add("icons");
-    let html = `<span class="material-icons-outlined copy" aria-label="copy" 
+    let html = `<span class="material-icons-outlined pin" aria-label="pin" 
+    tabindex="0" role="button">push_pin</span>
+    <span class="material-icons-outlined copy" aria-label="copy" 
     tabindex="0" role="button">content_copy</span>`
+    
+    // include share icon only if navigator.share is available
     if (navigator.canShare) {
         html += `<span class="material-icons-outlined share" aria-label="share" tabindex="0" 
     role="button">share</span>`
@@ -89,10 +113,12 @@ function addQuotation(quote, citation) {
     icons.innerHTML = html;
     blockquote.appendChild(icons);
 
+    // Add body of the quote
     const text = document.createElement("p");
     text.innerText = quote;
     blockquote.appendChild(text);
 
+    // Add citation
     const cite = document.createElement("footer");
     if (citation.length === 0) {
         citation = "Anonymous";
@@ -100,54 +126,59 @@ function addQuotation(quote, citation) {
     cite.innerText = `\u2014 ${citation}`;
     blockquote.appendChild(cite);
 
-    blockquote.addEventListener("click", (e) => {
-        if (e.target.classList.contains("copy")) {
-            copyQuote(e.currentTarget);
-        }
-        else if (e.target.classList.contains("share")) {
-            shareQuote(e.currentTarget);
-        }
-        else {
-            pinUnpinQuotation(e.currentTarget);
-        }
-    });
+    // Add event listeners to the new blockquote
+    blockquote.addEventListener("click", (e) => handleQuoteClick(e));
 
     blockquote.addEventListener("keypress", (e) => {
         if (e.code === "Enter") {
-            pinUnpinQuotation(e.currentTarget);
+            handleQuoteClick(e);
         }
     });
 
+    // For screen readers
     blockquote.addEventListener("focus", (e) => {
         readQuote(e.currentTarget);
     });
 
+    // Append to unpinned quotes
+    const quotations = document.getElementById("quotations");
     quotations.appendChild(blockquote);
 }
 
+function handleQuoteClick(e) {
+    // if icon specifically clicked, handle that request
+    if (e.target.classList.contains("copy")) {
+        copyQuote(e.currentTarget);
+    }
+    else if (e.target.classList.contains("share")) {
+        shareQuote(e.currentTarget);
+    }
+    // otherwise pin/unpin quote
+    else {
+        pinUnpinQuotation(e.currentTarget);
+    }
+}
+
+/************************************ 
+*   pin/unpin functionality 
+*/
 function pinUnpinQuotation(quote) {
     const quotations = document.getElementById("quotations");
     const pinned = document.getElementById("pinned-quotations");
     if (quotations.contains(quote)) {
+        // Remove quote from unpinned
         quotations.removeChild(quote);
 
-        const icons = quote.querySelector("div");
-        const span = document.createElement("span");
-        span.classList.add("material-icons-outlined");
-        span.innerText = "push_pin";
-        icons.insertBefore(span, icons.firstChild);
-
+        // Append to pinned
         pinned.appendChild(quote);
         quote.dataset.pinned = "true";
         displayMessage("Pinned", "");
     }
     else {
+        // Remove from pinned
         pinned.removeChild(quote);
-        const icons = quote.querySelector("div");
-        const span = icons.querySelector("span");
-        if (span) {
-            icons.removeChild(span);
-        }
+
+        // insert at top of unpinned
         if (quotations.firstChild) {
             quotations.insertBefore(quote, quotations.firstChild);
         }
@@ -156,10 +187,13 @@ function pinUnpinQuotation(quote) {
         }
         quote.dataset.pinned = "false";
         displayMessage("Unpinned", "");
-        searchText.focus();
     }
+    searchText.focus();
 }
 
+/************************************ 
+*   functionality to read quote aloud
+*/
 function readQuote(quote) {
     const quotations = document.getElementById("quotations");
     message = quote.innerText;
@@ -172,12 +206,16 @@ function readQuote(quote) {
     displayMessage(message, "");
 }
 
+/************************************ 
+*   functionality to handle icons in quotation blocks 
+*/
 async function copyQuote(quote) {
     const text = quote.querySelector("p").innerText;
     const cite = quote.querySelector("footer").innerText;
     const quotation =  `${text}\n\n${cite}`
     
     if (navigator.clipboard) {
+        // clipboard API is available
         try {
             await navigator.clipboard.writeText(quotation);
         } catch(error) {
@@ -186,6 +224,7 @@ async function copyQuote(quote) {
         }
     }
     else {
+        // if clipboard API is not available
         try {
             const textArea = document.createElement("textarea");
             textArea.textContent = quotation;
@@ -202,6 +241,7 @@ async function copyQuote(quote) {
 }
 
 async function shareQuote(quote) {
+    // If web share API is available, do sharing
     const text = quote.querySelector("p").innerText;
     const cite = quote.querySelector("footer").innerText;
     const quotation = `${text}\n\n${cite}`;
@@ -218,10 +258,15 @@ async function shareQuote(quote) {
     }
 }
 
+/************************************ 
+*   functionality to display and/or announce messages
+*/
 function displayMessage(message, show) {
     const errorBox = document.getElementById("message");
     errorBox.innerText = message;
+    // if show == "" message only announced by screen reader
     if (show === "message") {
+        // informational message to display
         const span = document.createElement("span");
         span.classList.add("material-icons-outlined");
         span.innerText = "info ";
@@ -229,6 +274,7 @@ function displayMessage(message, show) {
         errorBox.style.setProperty("top", "200px");
     }
     else if (show === "error") {
+        // error to display
         const span = document.createElement("span");
         span.classList.add("material-icons-outlined");
         span.innerText = "error_outline ";
@@ -244,11 +290,15 @@ function displayMessage(message, show) {
     }, 3000);
 }
 
+/************************************ 
+*   setup search bar and application 
+*/
 const baseURL = "https://usu-quotes-mimic.vercel.app/api/";
 const searchText = document.getElementById("search-text");
 let initialState = true;
 
 searchText.addEventListener("keydown", (e) => {
+    // deprecated method as it handles "Go" key on my phone properly?
     if (e.keyCode === 13) {
         if (e.target.value === "") {
             displayMessage("Specify author name to search", "error");
@@ -256,33 +306,30 @@ searchText.addEventListener("keydown", (e) => {
         }
 
         if (initialState) {
+            // first query
             const container = document.getElementById("container");
-            const searchBar = document.getElementById("search-bar");
             const header = document.getElementById("header");
+
+            // relocates search bar to fixed header
+            const searchBar = document.getElementById("search-bar");
             container.removeChild(searchBar);
             header.appendChild(searchBar);
+
+            // realigns main page content
             container.style.setProperty("justify-content", "flex-start");
             container.style.setProperty("margin-top", "78px");
             initialState = false;
         }
+
+        // service query
         getQuotes(e.target.value);
         e.target.focus();
     }
 });
 
-/* search settings dialog */
-let settingsDisplayed = false;
-let searchLevel = 1;
+// search bar icons (filter and clear search text)
 const searchSettings = document.getElementById("search-settings");
 const searchCancel = document.getElementById("search-cancel");
-
-function toggleSettingsDialog() {
-    settingsDisplayed = !settingsDisplayed;
-    const settings = document.getElementById("settings");
-    settings.dataset.open = `${settingsDisplayed}`;
-    const mask = document.getElementById("mask");
-    mask.dataset.open = `${settingsDisplayed}`;
-}
 
 searchSettings.addEventListener("click", () => {
     toggleSettingsDialog();
@@ -306,13 +353,31 @@ searchCancel.addEventListener("keypress", (e) => {
     }
 });
 
-const okButton = document.getElementById("ok-button");
-okButton.addEventListener("click", toggleSettingsDialog);
-
+// on loading page
 addEventListener("load", () => {
     getQuotes("");
     searchText.focus();
 });
+
+/************************************ 
+*   search settings dialog functionality 
+*/
+let settingsDisplayed = false;
+let searchLevel = 1;
+
+function toggleSettingsDialog() {
+    // display dialog
+    settingsDisplayed = !settingsDisplayed;
+    const settings = document.getElementById("settings");
+    settings.dataset.open = `${settingsDisplayed}`;
+
+    // app mask to make dialog modal
+    const mask = document.getElementById("mask");
+    mask.dataset.open = `${settingsDisplayed}`;
+}
+
+const okButton = document.getElementById("ok-button");
+okButton.addEventListener("click", toggleSettingsDialog);
 
 const searchOptions = [...document.getElementsByClassName("search-level")];
 searchOptions.forEach((option) => {
